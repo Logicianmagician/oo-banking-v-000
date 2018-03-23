8dfff1139973b0599a2cc8ff63892799e2e17666
Andrew Kairys <andrewkairys@gmail.com>
2018-03-23 16:42:29 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,8 +2,9 @@ class BankAccount
   attr_accessor :name
 
 
-  def initialize(bank_account)
+  def initialize(bank_account, balance=1000)
     @name = bank_account
+    @balance = balance
   end
 
 end
