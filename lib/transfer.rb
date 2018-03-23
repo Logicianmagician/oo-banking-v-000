08440d822aa024ecd9b79378ee9bb97609301d00
Andrew Kairys <andrewkairys@gmail.com>
2018-03-23 17:13:23 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,13 @@
 class Transfer
 
-  attr_reader :name
+  attr_reader :amount, :sender, :receiver
+  attr_accessor :status
 
-  def initialize(name)
-    @name = name
+  def initialize(status, sender, receiver, amount)
+    @status = "pending"
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
   end
 
 end
